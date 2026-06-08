54e446a867f6648a2a50d6f1314ca4f9d94035d5
laziz <qudratov.com26@gmail.com>
2026-06-08 17:39:38 +0500

    task 3 is completed

---
+++ task-3/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/qudratov_com_26_gmail_com", (req, res) => {
+  const rawX = req.query.x;
+  const rawY = req.query.y;
+  if (!Number.isInteger(Number(rawX)) || !Number.isInteger(Number(rawY))) {
+    return res.type("text/plain").send("NaN");
+  }
+  res.type("text/plain").send(lcm(rawX, rawY));
+});
+
+function lcm(x, y) {
+    return String(x * y / gcd(x, y));
+}
+
+function gcd(x, y) {
+  while (y !== 0) {
+    let temp = y;
+    y = x % y;
+    x = temp;
+  }
+  return x;
+}
