8d90d3314c43ba348c135cfb45e2e12e6be646b9
laziz <qudratov.com26@gmail.com>
2026-06-08 18:05:12 +0500

    should fix bot issue

--- task-3/server.js
+++ task-3/server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const port = process.env.PORT || 3000;
 
 app.get("/app/qudratov_com_26_gmail_com", (req, res) => {
   const rawX = req.query.x;
   const rawY = req.query.y;
-  if (!Number.isInteger(Number(rawX)) || !Number.isInteger(Number(rawY)) || Number(rawX) <= 0 || Number(rawY) <= 0) {
+
+  if (!/^\d+$/.test(rawX) || !/^\d+$/.test(rawY) || Number(rawX) <= 0 || Number(rawY) <= 0) {
     return res.type("text/plain").send("NaN");
   }
-  res.type("text/plain").send(lcm(rawX, rawY));
+
+  const x = Number(rawX);
+  const y = Number(rawY);
+
+  res.type("text/plain").send(lcm(x, y));
 });
 
 function lcm(x, y) {
-    return String(x * y / gcd(x, y));
+  return String(x * y / gcd(x, y));
 }
 
 function gcd(x, y) {
@@ -27,3 +28,7 @@ function gcd(x, y) {
   }
   return x;
 }
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
